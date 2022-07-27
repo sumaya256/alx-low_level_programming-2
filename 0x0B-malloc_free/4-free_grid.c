28d22e15734201f7718f910c9ced42a5450b9afd
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-07-27 22:59:34 +0300

    Update 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,13 +1,11 @@
 #include <stdlib.h>
-
 /**
- * free_grid - Free memory allocated to previoiusly created 2D array
- * @grid: The 2D array to free memory from
- * @height: The height of the 2D array
- *
- * Return: Nothing
- */
-
+* free_grid - Free memory allocated to previoiusly created 2D array
+* @grid: The 2D array to free memory from
+* @height: The height of the 2D array
+*
+* Return: Nothing
+*/
 void free_grid(int **grid, int height)
 {
 	int i;
